8cdde55b898ec0793497953d817ff5a27c4bd812
mehriddin-omon <mehriddinomonboyev@gmail.com>
2025-10-04 20:38:43 +0500

    tuzatish 4

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,6 @@ import { Logger } from '@nestjs/common';
 const isProd = process.env.NODE_ENV === 'production'; // hozir false bo'ladi
 
 async function bootstrap() {
-
-  if (isProd) {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const Sentry = require('@sentry/node');
-    Sentry.init({
-      dsn: process.env.SENTRY_DSN,
-      tracesSampleRate: 1.0,
-    });
-  }
-
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', 2003);
