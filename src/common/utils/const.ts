aca49a0be9c0a09b4db362f134339bb0bcaaafa2
mehriddin-omon <mehriddinomonboyev@gmail.com>
2025-10-05 16:40:17 +0500

    tuzatish 8

--- src/common/utils/const.ts
+++ src/common/utils/const.ts
@@ -1,6 +1,6 @@
 import { isProd } from "src/config/config";
 
-export const TELEGRAM_GROUP_ID = isProd ? '1896733920' : '-1002922948167';
+export const TELEGRAM_GROUP_ID = isProd ? '-1001896733920' : '-1002922948167';
 export const GROUP_URL = isProd ? 'https://t.me/english_with_Shamsiddinov' : 'https://t.me/bunyod_english_demobot';
 export const TEACHER_ID = [1355383611, 5129652174];
 
